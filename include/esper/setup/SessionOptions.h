2dec02259c77a70c4474725d6a09f031d0760067
Colin <why101@msn.com>
2020-01-03 13:10:07 -0800

    updated SessionOptions

--- include/esper/setup/SessionOptions.h
+++ include/esper/setup/SessionOptions.h
@@ -1,3 +1,12 @@
-struct SessionOptions {
-	bool keepMasterElementsUpdated;
-};
+#ifndef ESPER_SESSION_OPTIONS_H_
+#define ESPER_SESSION_OPTIONS_H_
+
+namespace esper {
+	struct SessionOptions {
+		bool keepMasterElementsUpdated = false;
+		bool allowLightPlugins = true;
+		bool emulateGlobalLoadOrder = true;
+	};
+}
+
+#endif
